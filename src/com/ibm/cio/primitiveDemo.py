d91c40bdcfa091a6caf45bbbed10e835c9bf5317
zgsun <zgsun@9.181.91.36>
2017-07-25 14:54:30 +0800

    primitive demo

---
+++ src/com/ibm/cio/primitiveDemo.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 25, 2017
+
+@author: zgsun
+'''
+import math
+
+if __name__ == '__main__':
+    print(2+4);
+    print(2.0+4);
+    print(10/5);
+    print(type(2));
+    print(type(2.0));
+    print(type(5566734534654543626556425675364365435435436454));
+    print(abs(-10)) #绝对值
+    print(round(1.55))#四舍五入
+    print(math.pow(3, 3))
