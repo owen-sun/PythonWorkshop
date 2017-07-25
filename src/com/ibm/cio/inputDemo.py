6146a489bbe07753959426979abec66e2d09acc7
zgsun <zgsun@9.181.91.36>
2017-07-25 14:05:35 +0800

    input demo

---
+++ src/com/ibm/cio/inputDemo.py
@@ -0,0 +1,11 @@
+'''
+Created on Jul 25, 2017
+
+@author: zgsun
+'''
+
+if __name__ == '__main__':
+    name = input("please enter your name: ");
+    print("hello: " + name);
+    org=input("please enter your organize name: ");
+    print(org + "has been created successfully!");
