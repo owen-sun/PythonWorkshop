c6b7c97631d10f4548145c5046298db5ee2d22d2
zgsun <zgsun@9.181.91.36>
2017-07-25 15:29:38 +0800

    string demo

---
+++ src/com/ibm/cio/stringDemo.py
@@ -0,0 +1,19 @@
+'''
+Created on Jul 25, 2017
+
+@author: zgsun
+'''
+
+
+if __name__ == '__main__':
+    print("今天是个好天气！");
+    print("'今天'是个好天气！");
+    print("what's your name?");
+    name='owen sun';
+    print(name.upper());
+    print(name);
+    print(name.lower());
+    print(name.istitle());
+    print(name.capitalize());
+    print(name.title());
+    print(name[-3])
